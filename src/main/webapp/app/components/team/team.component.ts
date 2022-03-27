2e4014a200342d9e90e44740a78cd751b686eeae
Lokilicious <matthias.vavti@gmail.com>
2022-03-27 15:54:22 +0200

    Save

--- src/main/webapp/app/components/team/team.component.ts
+++ src/main/webapp/app/components/team/team.component.ts
@@ -5,6 +5,10 @@ import { Team } from '../../entities/team/team.model';
 import { ApiService } from '../../services/api.service';
 import { Survey } from '../../entities/survey/survey.model';
 import { map } from 'rxjs/operators';
+import { take } from 'cypress/types/lodash';
+import { IQuestion, Question } from 'app/entities/question/question.model';
+import { Answer } from 'app/entities/answer/answer.model';
+import { Dayjs } from 'dayjs/esm';
 
 @Component({
   selector: 'jhi-team',
@@ -14,6 +18,10 @@ import { map } from 'rxjs/operators';
 export class TeamComponent implements OnInit {
   public teamId = '';
   public surveys$: Observable<Survey[]> | undefined;
+  public questions: Array<IQuestion | undefined | null> = [];
+  public answerMap: Map<Dayjs | null, Answer> = new Map;
+  public surveyDates: Array<Date> = [];
+
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {
   }
@@ -23,8 +31,32 @@ export class TeamComponent implements OnInit {
       if(params.id) {
         this.teamId = params.id;
 
-        this.surveys$ = this.apiService.getSurveys().pipe(map(surveys => surveys.filter(s => s.team?.id === this.teamId).slice(0, 5)));
+        this.surveys$ = this.apiService.getSurveys().pipe(map(surveys => surveys.filter(s => s.team?.id === this.teamId).slice(0, 3)));
+
+       
+        this.surveys$.subscribe(surveys => {
+          surveys[0].answers?.forEach(a => {
+            if(a.question !== undefined) {
+              this.questions.push(a.question);
+            }
+              //this.answerMap.set(a.question, a);
+          });
+
+        
+
+          surveys.forEach(s => {
+            s.answers?.forEach(a => {
+              if(s.surveyDate !== undefined) {
+                this.answerMap.set(s.surveyDate, a);
+              }
+                
+            })
+          });           
+           
+         });
+
+
       }
-    })
+    });
   }
 }
